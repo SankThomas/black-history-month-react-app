59d59f1bf84b2d1243ce3a8e358da2933306266b
Thomas Sankara <thomassankara74@gmail.com>
2021-02-19 16:55:19 +0300

    install dependencies, set up initial routing

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className="logo">
+          <h2>Logo</h2>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/Titles.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Titles = () => {
+  return (
+    <>
+      <h1>Fetch Data here</h1>
+    </>
+  )
+}
+
+export default Titles
